82afeb13f554f155506a563ce47e319edd6a7f62
byron <47872200+byeoon@users.noreply.github.com>
2024-02-14 20:07:10 -0500

    i might be super dumb

--- src/index.tsx
+++ src/index.tsx
@@ -50,7 +50,7 @@ const EmotesPlus: Plugin = {
                      showToast("Copied emote url to clipboard.");
                      console.log("[EmotesPlus] a thing: " + emojiNode.src);
                      Clipboard.setString(emojiNode.src);
-                     res?.props?.children?.props?.children?.props?.children.push(<Button text='Copy Emote URL'/>)
+                     res.push(<Button text='Copy Emote URL'/>)
                      showToast("what the FACK.");
                     
                   }
