02edcd3ae2767d81b223d0795671b4225209de7a
byron <47872200+byeoon@users.noreply.github.com>
2024-03-01 18:22:13 -0500

    rosie from rosiecord

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,17 @@ import { Plugin, registerPlugin } from 'enmity/managers/plugins';
 import { React, Dialog, Toasts } from 'enmity/metro/common';
 import { getByProps } from 'enmity/metro';
 import { create } from 'enmity/patcher';
-import { Button } from "enmity/components";
+import { Button, Text, TouchableOpacity } from "enmity/components";
 import manifest from '../manifest.json';
 import Settings from './components/Settings';
 import EmotesSheet from './components/EmotesSheet';
 import findInReactTree from 'enmity/utilities/findInReactTree';
 
 
-const ActionSheet = getByProps("openLazy", "hideActionSheet");
+const LazyActionSheet = getByProps("openLazy", "hideActionSheet");
 const Clipboard = getByProps('setString');
 const Patcher = create('EmotesPlus');
+
 
 function showDialog(text) {
    Dialog.show({
@@ -33,29 +34,30 @@ const EmotesPlus: Plugin = {
    onStart() {
       console.log("[EmotesPlus] Hello World!");
 
-      Patcher.before(ActionSheet, "openLazy", (_, [component, sheet]) => {
-         if (sheet === "MessageEmojiActionSheet") {
-            component.then((instance) => {
-               const unpatch = Patcher.after(instance, "default", (_, [{ emojiNode }], res) => {
-                  React.useEffect(() => unpatch(), [])
-                  findInReactTree(res, (node) => console.log(node))
-                  if (true) {
-                     console.log("[EmotesPlus] IT DID A THING!!!!!");
-                     
-                     showToast("Copied emote url to clipboard.");
-                     console.log("[EmotesPlus] a thing: " + emojiNode.src);
-                     Clipboard.setString(emojiNode.src);
-                     (res.props.children ??=[]).push(<Button key="copyEmoteUrlButton" text='Copy Emote URL'/>); // thank u eternal
- 
-                     showToast("UI push should have worked but it never does lmao.");
-                     console.log("[EmotesPlus] " + res.props);
-                  }
-                  return res
-                
-               })
-            })
-         }
-      })
+      const unpatchOpenLazy = Patcher.before(LazyActionSheet, 'openLazy', (_, [component, key]) => {
+         if (key !== 'MessageEmojiActionSheet') return;
+         unpatchOpenLazy();
+     
+         component.then(instance => {
+             const unpatchInstance = Patcher.after(instance, 'default', (_, __, res) => {
+                 unpatchInstance();
+     
+                 const unpatchType = Patcher.after(res, 'type', (_, __, res) => {
+                     React.useEffect(() => () => void unpatchType(), []);
+                     const details = findInReactTree(res, x => x?.type && x?.props?.emojiNode && x?.props?.nonce);
+                     if (!details) return;
+     
+                     Patcher.after(details, 'type', (_, __, res) => {
+                         res?.props?.children?.push(
+                             <TouchableOpacity onPress={() => console.log('you pressed it')}>
+                                 <Text>Press me!</Text>
+                             </TouchableOpacity>
+                         );
+                     })
+                 })
+             })
+         })
+     })
    },
 
    onStop() {
